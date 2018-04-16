1454c085708134a3b0b444120a136b3373068e02
Unknown <cmolln10@student.aau.dk>
2018-04-16 15:23:44 +0200

    Rough first prototype.

---
+++ Assets/Scripts/FiringManagement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FiringManagement : MonoBehaviour {
+	public UnityEvent fire;
+	bool firing = false;
+	// Use this for initialization
+	void Start () {
+		fire = new UnityEvent();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.K) && !firing)
+		{
+			StartCoroutine(AutoFire(0.3f));
+		}
+
+	}
+
+	IEnumerator AutoFire(float checktime)
+	{
+		firing = true;
+		while (firing)
+		{
+			fire.Invoke();
+
+			yield return new WaitForSeconds(checktime);
+
+			firing = Input.GetKey(KeyCode.K);
+
+		}
+
+	}
+
+}
