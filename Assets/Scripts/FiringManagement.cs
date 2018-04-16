90175d362914574aef13e9ea6d1d9488f3a0110a
Unknown <cmolln10@student.aau.dk>
2018-04-16 20:43:08 +0200

    Dual mode

--- Assets/Scripts/FiringManagement.cs
+++ Assets/Scripts/FiringManagement.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+
+public enum GunState
+{
+    Overheat,
+    Normal
+}
 
 public class FiringManagement : MonoBehaviour {
 	public UnityEvent fire;
 	bool firing = false;
+
+    GunState gunState = GunState.Normal;
+
+    float checktime = 0.3f;
+    float heat = 0;
+    public Slider slider;
+
 	// Use this for initialization
 	void Start () {
 		fire = new UnityEvent();
+        fire.AddListener(SetSlider);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.K) && !firing)
 		{
-			StartCoroutine(AutoFire(0.3f));
+			StartCoroutine(AutoFire(ModeToWeaponCoolDownTime(gunState)));
 		}
 
 	}
@@ -25,14 +40,48 @@ public class FiringManagement : MonoBehaviour {
 		firing = true;
 		while (firing)
 		{
+            
 			fire.Invoke();
-
-			yield return new WaitForSeconds(checktime);
+            SetGunState();
+            yield return new WaitForSeconds(ModeToWeaponCoolDownTime(gunState));
 
 			firing = Input.GetKey(KeyCode.K);
 
 		}
 
 	}
+
+    void SetSlider()
+    {
+        slider.value = heat;
+        
+    }
+
+    float ModeToWeaponCoolDownTime(GunState state)
+    {
+        if(state == GunState.Overheat)
+        {
+            return 1f;
+        }
+        else
+        {
+            return 0.3f;
+        }
+    }
+
+    void SetGunState()
+    {
+        if(heat < 1f)
+        {
+            heat += 0.1f;
+            gunState = GunState.Normal;
+        }
+        else
+        {
+            heat -= 0.5f;
+            gunState = GunState.Overheat;
+
+        }
+    }
 
 }
